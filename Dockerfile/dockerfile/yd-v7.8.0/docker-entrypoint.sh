3504655b072e9c61ac0d4a6f865bb988913524b2
Jinjin123 <1293813551@qq.com>
2019-02-01 08:12:43 +0800

    split env

--- Dockerfile/dockerfile/yd-v7.8.0/docker-entrypoint.sh
+++ Dockerfile/dockerfile/yd-v7.8.0/docker-entrypoint.sh
@@ -6,6 +6,8 @@ export PATH=$PATH:/usr/local/.nvm/versions/node/v7.8.0/bin
 CODE_PATH='/var/www/html'
 MONITOR='/var/www'
 /bin/cp $MONITOR/newrelic.js  $CODE_PATH/newrelic.js
+ENV NPM_PATH /usr/local/.nvm/versions/node/v7.8.0/bin/npm
+ENV NODE_PATH /usr/local/.nvm/versions/node/v7.8.0/bin/node
 cd $CODE_PATH
 /bin/sed -i "s/app_name: .*/app_name: [\"$NEWRELICAPPNAME\"],/"   $CODE_PATH/newrelic.js
 /bin/sed -i "s/license_key: .*/license_key: \"$NEWRELICKEY\",/"   $CODE_PATH/newrelic.js
@@ -13,20 +15,20 @@ if [ -e index.js ]
 then
 	/bin/sed -ie '/newrelic/d' $CODE_PATH/index.js
 	/bin/sed -i '1 irequire("newrelic")' $CODE_PATH/index.js
-	cd $CODE_PATH &&  /usr/local/.nvm/versions/node/v7.8.0/bin/npm  install
+	cd $CODE_PATH &&  $NPM_PATH  install
 	if [ $? -ne 0 ]
 	then
-	  	/usr/local/.nvm/versions/node/v7.8.0/bin/npm  install  newrelic && /usr/local/.nvm/versions/node/v7.8.0/bin/node  $CODE_PATH/index.js
+	  	$NPM_PATH  install  newrelic && $NODE_PATH  $CODE_PATH/index.js
 	else
-	  	/usr/local/.nvm/versions/node/v7.8.0/bin/npm  install  newrelic && /usr/local/.nvm/versions/node/v7.8.0/bin/node  $CODE_PATH/index.js
+	  	$NPM_PATH  install  newrelic && $NODE_PATH  $CODE_PATH/index.js
 	fi
 elif [ -e app.js ]
 then
 	/bin/sed -ie '/newrelic/d' $CODE_PATH/app.js
 	/bin/sed -i '1 irequire("newrelic")' $CODE_PATH/app.js
-	cd $CODE_PATH && /usr/local/.nvm/versions/node/v7.8.0/bin/npm  install && /usr/local/.nvm/versions/node/v7.8.0/bin/npm  install  newrelic &&  /usr/local/.nvm/versions/node/v7.8.0/bin/node  $CODE_PATH/app.js
+	cd $CODE_PATH && $NPM_PATH  install && $NPM_PATH  install  newrelic &&  $NODE_PATH  $CODE_PATH/app.js
 else
 	/bin/sed -ie '/newrelic/d' dist/index.js
 	/bin/sed -i '1 irequire("newrelic")' dist/app.js
-	cd $CODE_PATH && /usr/local/.nvm/versions/node/v7.8.0/bin/npm  install && /usr/local/.nvm/versions/node/v7.8.0/bin/npm  install  newrelic &&  /usr/local/.nvm/versions/node/v7.8.0/bin/node  dist/app.js
+	cd $CODE_PATH && $NPM_PATH  install && $NPM_PATH  install  newrelic &&  $NODE_PATH  dist/app.js
 fi
